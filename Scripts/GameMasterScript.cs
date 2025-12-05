e2b19bae0515c628bc8bf38a9e24a68b0646a45c
Billy Leong <104392674+TatWof@users.noreply.github.com>
2025-12-05 09:25:19 -0800

    bug fixing

--- Scripts/GameMasterScript.cs
+++ Scripts/GameMasterScript.cs
@@ -64,6 +64,10 @@ public class GameMasterScript : MonoBehaviour
             {
                 PauseGame();
             }
+            else
+            {
+                RefocusGame();
+            }
 
         }
 
